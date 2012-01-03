5f831100743095d17ff2e41de9870d80df5c570f
Herrmann, Tom <linux@randoom.org>
2012-01-03 19:40:22 +0100

    improve handling of strange parser behavior

--- setlX/grammar_pure/ParsePure.java
+++ setlX/grammar_pure/ParsePure.java
@@ -8,13 +8,15 @@ public class ParsePure {
         CommonTokenStream   ts      = new CommonTokenStream(lexer);
         PureParser          parser  = new PureParser(ts);
 
+        int                 errors  = 0;
+
         parser.initBlock();
 
         // fill token stream until EOF is reached (parser fills only as far as its lookahead needs)
         ts.fill();
         // current index in stream of tokens
         int index = ts.index();
-        
+
         /*
          *  If the index into the tokenStream (which was set by the parser)
          *  is not equal to the number of tokens in the complete input (minus EOF),
@@ -25,19 +27,21 @@ public class ParsePure {
         if (index < (ts.size() - 1)) {
             // parse again to force displaying syntax error in remaining tokenStream
             parser.initBlock();
-            
+
             // check if parser moved the index
             if (index == ts.index()) {
                 /*  Index was not moved. Probably epsilon can be derived from the start-rule.
                  *  However there are still tokens left...
                  */
-                System.err.println("ParsePure: Input includes unidentified errors!");
+                errors++;
+                Token t = ts.get(index);
+                System.err.println("line " + t.getLine() + ":" + t.getCharPositionInLine() + " input '" + ts.toString(index, ts.size()) + "' includes unidentified errors");
             }
         }
 
-        int errors = parser.getNumberOfSyntaxErrors();
+        errors += parser.getNumberOfSyntaxErrors() + lexer.getNumberOfSyntaxErrors();
         if (errors > 0) {
-            System.err.println("ParsePure: " + errors + " syntax errors encountered in the input!");
+            System.err.println("ParsePure: At least " + errors + " syntax error(s) were encountered in the input!");
         }
     }
 }
--- setlX/src/java-src/interpreter/utilities/ParseSetlX.java
+++ setlX/src/java-src/interpreter/utilities/ParseSetlX.java
@@ -18,15 +18,12 @@ import java.io.IOException;
 
 public class ParseSetlX {
 
-    private final	static  int EXPR    = 1337;
-    private final	static  int BLOCK   = 31337;
-
     public static Block parseFile(String fileName) throws ParserException {
         try {
             // parse the file contents (Antlr will print its parser errors into stderr ...)
-            return (Block) parseFragment(new ANTLRFileStream(fileName), BLOCK);
+            return parseBlock(new ANTLRFileStream(fileName));
 
-        } catch (IOException e) {
+        } catch (IOException ioe) {
             throw new FileNotReadableException("File '" + fileName + "' could not be read.");
         }
     }
@@ -36,7 +33,7 @@ public class ParseSetlX {
             InputStream         stream = InputReader.getStream();
 
             // parse the input (Antlr will print its parser errors into stderr ...)
-            return (Block) parseFragment(new ANTLRInputStream(stream), BLOCK);
+            return parseBlock(new ANTLRInputStream(stream));
 
         } catch (IOException ioe) {
             throw new EndOfFileException("eof");
@@ -48,7 +45,7 @@ public class ParseSetlX {
             InputStream         stream = new ByteArrayInputStream(input.getBytes());
 
             // parse the input (Antlr will print its parser errors into stderr ...)
-            return (Block) parseFragment(new ANTLRInputStream(stream), BLOCK);
+            return parseBlock(new ANTLRInputStream(stream));
 
         } catch (IOException ioe) {
             throw new EndOfFileException("eof");
@@ -60,26 +57,52 @@ public class ParseSetlX {
             InputStream         stream = new ByteArrayInputStream(input.getBytes());
 
             // parse the input (Antlr will print its parser errors into stderr ...)
-            return (Expr) parseFragment(new ANTLRInputStream(stream), EXPR);
+            return parseExpr(new ANTLRInputStream(stream));
 
         } catch (IOException ioe) {
             throw new EndOfFileException("eof");
         }
     }
 
-    private static CodeFragment parseFragment(ANTLRStringStream input, int type) throws SyntaxErrorException {
+    /* private methods */
+
+    private final   static  int EXPR    =  1337;
+    private final   static  int BLOCK   = 31337;
+
+    private static Block parseBlock(ANTLRStringStream input) throws SyntaxErrorException {
+        return (Block) handleFragmentParsing(input, BLOCK);
+    }
+
+    private static Expr parseExpr(ANTLRStringStream input) throws SyntaxErrorException {
+        return (Expr) handleFragmentParsing(input, EXPR);
+    }
+
+    private static CodeFragment parseFragment(SetlXgrammarParser parser, int type) throws RecognitionException {
+        switch (type) {
+            case EXPR:
+                return parser.anyExpr();
+            case BLOCK:
+                return parser.initBlock();
+            default:
+                /* this should never be reached if surrounding code is correct */
+                return null;
+        }
+    }
+
+    private static CodeFragment handleFragmentParsing(ANTLRStringStream input, int type) throws SyntaxErrorException {
+        SetlXgrammarLexer   lexer   = null;
         SetlXgrammarParser  parser  = null;
         try {
-            SetlXgrammarLexer   lexer  = new SetlXgrammarLexer(input);
+                                lexer  = new SetlXgrammarLexer(input);
             CommonTokenStream   ts     = new CommonTokenStream(lexer);
                                 parser = new SetlXgrammarParser(ts);
 
             // parse the input
-            CodeFragment        frag   = readFragment(parser, type);
+            CodeFragment        frag   = parseFragment(parser, type);
 
             // now Antlr will print its parser errors into stderr ...
 
-            /*     check for syntax errors at the end of the input stream     */
+            /* check for unparsed syntax errors at the end of the input stream */
 
             // fill token stream until EOF is reached (parser fills only as far as its lookahead needs)
             ts.fill();
@@ -88,53 +111,58 @@ public class ParseSetlX {
 
             /*
              *  If the index into the tokenStream (which was set by the parser)
-             *  is not equal to the number of tokens in the complete input (minus EOF),
-             *  some tokens where ignored by the parser.
+             *  is not equal to the number of tokens in the complete input
+             *  (minus EOF), some tokens where ignored by the parser.
              *
              *  Best guess: The parser encountered a syntax error after a valid rule.
              */
             if (index < (ts.size() - 1)) {
                 // parse again to force displaying syntax error in remaining tokenStream
-                readFragment(parser, type);
+                parseFragment(parser, type);
 
                 // now Antlr will (again) print its parser errors into stderr ...
 
                 // check if parser moved the index
                 if (index == ts.index()) {
-                    /*  Index was not moved. Probably epsilon can be derived from the start-rule.
-                     *  However there are still tokens left...
-                     *  THIS SHOULD NOT BE POSSIBLE IN SetlX!
+                    /*  Index was not moved. Probably epsilon can be and was
+                     *  derived from the start-rule.
+                     *  However there are still unparsed tokens left...
+                     *
+                     *  Note: In SetlX this can NEVER be the case, because
+                     *        epsilon can NOT be derived from any start-rule!
                      */
-                    throw new SyntaxErrorException("Input includes unidentified errors!");
+                    Token   t       = ts.get(index);
+                    String  error   = "line " + t.getLine() + ":" + t.getCharPositionInLine();
+                            error  += " input '" + ts.toString(index, ts.size()) + "' includes unidentified errors";
+                    // fake Antlr like error message
+                    System.err.println(error);
+                    // and stop parsing
+                    throw new SyntaxErrorException(error);
                 }
             }
 
-            if (parser.getNumberOfSyntaxErrors() > 0) {
-                throw new NullPointerException(); // different problem, but same handling as NullPointerException
+            int errors  = parser.getNumberOfSyntaxErrors() + lexer.getNumberOfSyntaxErrors();
+            if (errors > 0) {
+                throw new SyntaxErrorException("" + errors + " syntax error(s) encountered.");
             }
 
             return frag;
         } catch (RecognitionException re) {
             throw new SyntaxErrorException(re.getMessage());
         } catch (NullPointerException npe) {
-            if (parser != null && parser.getNumberOfSyntaxErrors() > 0) {
-                // NullPointerException caused by syntax error (or thrown because of errors)
-                throw new SyntaxErrorException("" + parser.getNumberOfSyntaxErrors() + " syntax errors encountered.");
+            int errors  = 0;
+            if (lexer != null) {
+                errors += lexer.getNumberOfSyntaxErrors();
+            }
+            if (parser != null) {
+                errors += parser.getNumberOfSyntaxErrors();
+            }
+            if (errors > 0) {
+                // NullPointerException caused by syntax error(s)
+                throw new SyntaxErrorException("" + errors + " syntax error(s) encountered.");
             } else { // NullPointer in parse tree itself
                 throw new SyntaxErrorException("Parsed tree contains nullpointer.");
             }
-        }
-    }
-
-    private static CodeFragment readFragment(SetlXgrammarParser parser, int type) throws RecognitionException {
-        switch (type) {
-            case EXPR:
-                return parser.anyExpr();
-            case BLOCK:
-                return parser.initBlock();
-            default:
-                /* this should never be reached if above code is correct */
-                return null;
         }
     }
 }

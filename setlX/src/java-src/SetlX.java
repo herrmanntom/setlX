9ac857e18c15a987b6cf078c0fec2817aea7ef49
Herrmann, Tom <linux@randoom.org>
2012-01-12 22:19:49 +0100

    include queens example and bump version to reflect new branch

--- setlX/src/java-src/SetlX.java
+++ setlX/src/java-src/SetlX.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 public class SetlX {
 
-    private final static String VERSION         = "0.3.3";
+    private final static String VERSION         = "0.4.0";
     private final static String VERSION_PREFIX  = "v";
     private final static String HEADER          = "-====================================setlX====================================-";
 

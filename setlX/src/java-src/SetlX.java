0fc4f3244b0866f737c93e03b11e78f9b804e2f8
Herrmann, Tom <linux@randoom.org>
2012-04-17 20:27:20 +0200

    modified example code; new version;

--- setlX/src/java-src/SetlX.java
+++ setlX/src/java-src/SetlX.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 public class SetlX {
 
-    private final static String VERSION         = "0.7.0";
+    private final static String VERSION         = "0.7.1";
     private final static String VERSION_PREFIX  = "v";
     private final static String HEADER          = "-====================================setlX====================================-";
 

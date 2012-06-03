41ac8e92ef7ff46fdc08f98d322c37edb4a0f476
Herrmann, Tom <linux@randoom.org>
2012-06-03 17:20:12 +0200

    update logo function

--- setlX/src/java-src/org/randoom/setlx/functions/PD_logo.java
+++ setlX/src/java-src/org/randoom/setlx/functions/PD_logo.java
@@ -26,42 +26,66 @@ import java.util.List;
 public class PD_logo extends PreDefinedFunction {
     public  final static PreDefinedFunction DEFINITION  = new PD_logo();
     private final static String[]           COINS       = {"Penny", "Nickel", "Dime", "Quarter"};
-    private final static String             LOGO        = "\n" +
-    "                                                                       " + "\n" +
-    "        .mO0C0O0M0P0U0T0E0R0S000M0A0K0E000V0E0R0Y000F0A0S0T0om.        " + "\n" +
-    "       .OOOOOOOOOOOOOOOOOOOOOO0S0E0T0L0X0OOOOOOOOOOOOOOOOOOOOOO.       " + "\n" +
-    "       OOO'                                                 'OOO       " + "\n" +
-    "      |OOO                                                   OOO|      " + "\n" +
-    "      |OOO                      =W,       dg.                OOO|      " + "\n" +
-    "      |OOO                       4Wc.     'WWc               OOO|      " + "\n" +
-    "      |OOO                        ?$mi     .WW6              OOO|      " + "\n" +
-    "      |OOO         _____           jWWg,   W 'W              OOO|      " + "\n" +
-    "      |OOO      .wWWWWWWWwa.      jW -WWw, W                 OOO|      " + "\n" +
-    "      |OOO     jW       -WWWi    [Wf   -WWwm      .aaaa.     OOO|      " + "\n" +
-    "     .OOO'    .WWw.      )WWW    [Wk     'WWc    ,W    W,    'OOO.     " + "\n" +
-    "  .amOOOP     'WWW'      .WWW;    ?Wg,     'Wm,  WW    WW     $OOOma.  " + "\n" +
-    "  OOOOO{        *        jWWW      WWW       WW  WW    WW      }OOOOO  " + "\n" +
-    "  'YOOOO,               _WWW`   -'??'`        ^  '@    @'     $OOOOY'  " + "\n" +
-    "     'OOO.             sWWW                       'aaaa'     .OOO'     " + "\n" +
-    "      |OOO           _yWW/                                   OOO|      " + "\n" +
-    "      |OOO         .wWW/                                     OOO|      " + "\n" +
-    "      |OOO        sWW/                                       OOO|      " + "\n" +
-    "      |OOO      _dW/      _.M                                OOO|      " + "\n" +
-    "      |OOO    _wWWWWWWWWWWWWM                                OOO|      " + "\n" +
-    "      |OOO    ''''''''''''''`                                OOO|      " + "\n" +
-    "      |OOO                                                   OOO|      " + "\n" +
-    "       OOO.                                                 .OOO       " + "\n" +
-    "       'OOOOOOOOOOOOOOOOOOOOOO0S0E0T0L0X0OOOOOOOOOOOOOOOOOOOOOO'       " + "\n" +
-    "        'YOOO0V0E0R0Y000A0C0C0U0R0A0T0E000M0I0S0T0A0K0E0S0OOOY'        " + "\n" +
-    "                                                                       " + "\n";
+    private final static String             LOGO_BIG    = "\n" +
+    "                                                                    " + "\n" +
+    "        .mO0C0O0M0P0U0T0E0R0S000M0A0K0E000V0E0R0Y000F0A0S0T0om.     " + "\n" +
+    "       .OOOOOOOOOOOOOOOOOOOOOO0S0E0T0L0X0OOOOOOOOOOOOOOOOOOOOOO.    " + "\n" +
+    "       OOO'                                                 'OOO    " + "\n" +
+    "       OOO                                                   OOO    " + "\n" +
+    "       OOO                      =W,       dg.                OOO    " + "\n" +
+    "       OOO                       4Wc.     'WWc               OOO    " + "\n" +
+    "       OOO                        ?$mi     .WW6              OOO    " + "\n" +
+    "       OOO         _____           jWWg,   W 'W              OOO    " + "\n" +
+    "       OOO      .wWWWWWWWwa.      jW -WWw, W                 OOO    " + "\n" +
+    "       OOO     jW       -WWWi    [Wf   -WWwm      .aaaa.     OOO    " + "\n" +
+    "     .OOO'    .WWw.      )WWW    [Wk     'WWc    ,W    W,    'OOO.  " + "\n" +
+    "   .aOOOP     'WWW'      .WWW;    ?Wg,     'Wm,  WW    WW     $OOOa." + "\n" +
+    "   OOOO{        *        jWWW      WWW       WW  WW    WW      }OOOO" + "\n" +
+    "   'YOOO,               _WWW`   -'??'`        ^  '@    @'     $OOOY'" + "\n" +
+    "     'OOO.             sWWW                       'aaaa'     .OOO'  " + "\n" +
+    "       OOO           _yWW/                                   OOO    " + "\n" +
+    "       OOO         .wWW/                                     OOO    " + "\n" +
+    "       OOO        sWW/                                       OOO    " + "\n" +
+    "       OOO      _dW/      _.M                                OOO    " + "\n" +
+    "       OOO    _wWWWWWWWWWWWWM                                OOO    " + "\n" +
+    "       OOO    ''''''''''''''`                                OOO    " + "\n" +
+    "       OOO                                                   OOO    " + "\n" +
+    "       OOO.                                                 .OOO    " + "\n" +
+    "       'OOOOOOOOOOOOOOOOOOOOOO0S0E0T0L0X0OOOOOOOOOOOOOOOOOOOOOO'    " + "\n" +
+    "        'YOOO0V0E0R0Y000A0C0C0U0R0A0T0E000M0I0S0T0A0K0E0S0OOOY'     " + "\n" +
+    "                                                                    " + "\n";
+
+    private final static String             LOGO_SMALL  = "\n" +
+    "                                                   " + "\n" +
+    "     .oooooooooooooooooooooooooooooooooooooooo.    " + "\n" +
+    "     OOOOOOOOOOOOOOO0S0E0T0L0X0OOOOOOOOOOOOOOOO    " + "\n" +
+    "    |OO'                                    'OO|   " + "\n" +
+    "    |OO                 mm     m.            OO|   " + "\n" +
+    "    |OO                 'Ww    'Ww.          OO|   " + "\n" +
+    "    |OO                  -Wwc   'WWw         OO|   " + "\n" +
+    "    |OO     .wWWWWWw     .W Ww. W 'Y         OO|   " + "\n" +
+    "    |OO    wW     'Ww.   WW  YW.d    .wWw,   OO|   " + "\n" +
+    "   .OOO   wWWw     WWw   WW,  'Yw.   W' 'W   OOO.  " + "\n" +
+    " .oOOY'   'WWW     WWY    WWm  'Yw   W   W   'YOOo." + "\n" +
+    " 'YOOo.           iWY   -mY'     W   W. .W   .oOOY'" + "\n" +
+    "   'OOO         .wW'                  YWY    OOO'  " + "\n" +
+    "    |OO       .wW'                           OO|   " + "\n" +
+    "    |OO     .wW'    .M                       OO|   " + "\n" +
+    "    |OO    mWWWWWWWWWM                       OO|   " + "\n" +
+    "    |OO   ''''''''''''                       OO|   " + "\n" +
+    "    |OO                                      OO|   " + "\n" +
+    "     OOOooooooooooooooooooooooooooooooooooooOOO    " + "\n" +
+    "     'OOOOOOOOOOOOOO0S0E0T0L0X0OOOOOOOOOOOOOOO'    " + "\n" +
+    "                                                   " + "\n";
 
     private PD_logo() {
         super("logo");
     }
 
     public Value execute(List<Value> args, List<Value> writeBackVars) {
-        int payUp = 0;
-        while(payUp <= 0 && payUp > -5) {
+        String  logo  = LOGO_SMALL;
+        int     payUp = 0;
+        while(payUp <= 0 && payUp > -3) {
             try {
                 payUp--;
                 Environment.prompt("Insert USD-Coin: ");
@@ -82,15 +106,23 @@ public class PD_logo extends PreDefinedFunction {
         if (payUp <= 0) {
             Environment.outWriteLn("Too bad... here a `penny' for your thoughts.");
             return new Rational(0);
-        } else if (payUp < COINS.length) {
+        } else if (payUp == 1) {
             Environment.outWriteLn("  ...cheap bastard...  ");
+            logo = LOGO_SMALL;
+        } else if (payUp == 2) {
+            Environment.outWriteLn("  ...well... never mind...  ");
+            logo = LOGO_SMALL;
+        }  else if (payUp == 3) {
+            Environment.outWriteLn("  ...you can to one better...  ");
+            logo = LOGO_BIG;
         } else {
             Environment.outWriteLn("Thank You!");
+            logo = LOGO_BIG;
         }
 
         int timeSum = 0;
-        for (int i = 0; i < LOGO.length(); i++) {
-            Environment.outWrite("" + LOGO.charAt(i));
+        for (int i = 0; i < logo.length(); i++) {
+            Environment.outWrite("" + logo.charAt(i));
             try {
                 int time = (i / (10 * payUp)) +1;
                 if (time > 125) {

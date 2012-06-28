e833d8c2438e3ca8bd86f51a1447848833a60b4c
Herrmann, Tom <linux@randoom.org>
2012-06-28 22:21:39 +0200

    added compare() function

---
+++ setlX/src/java-src/org/randoom/setlx/functions/PD_compare.java
@@ -0,0 +1,28 @@
+package org.randoom.setlx.functions;
+
+import org.randoom.setlx.types.Rational;
+import org.randoom.setlx.types.Value;
+
+import java.util.List;
+
+// compare(valueA, valueB)       : Compares two arbitrary values, returns an integer.
+//                                 Return value <  0 means: valueA "<" valueB
+//                                 Return value == 0 means: valueA ==  valueB
+//                                 Return value >  0 means: valueA ">" valueB
+//                                 Note that order between different types was chosen
+//                                 arbitrarily and implies no special meaning.
+
+public class PD_compare extends PreDefinedFunction {
+    public final static PreDefinedFunction DEFINITION = new PD_compare();
+
+    private PD_compare() {
+        super("compare");
+        addParameter("valueA");
+        addParameter("valueB");
+    }
+
+    public Value execute(List<Value> args, List<Value> writeBackVars) {
+        return new Rational(args.get(0).compareTo(args.get(1)));
+    }
+}
+

ea173084040c51e6154dafc5c2d0f969931868f1
Herrmann, Tom <linux@randoom.org>
2012-09-05 16:53:00 +0200

    remove unneeded includes

--- setlX/src/java-src/org/randoom/setlx/statements/MatchAbstractBranch.java
+++ setlX/src/java-src/org/randoom/setlx/statements/MatchAbstractBranch.java
@@ -1,7 +1,6 @@
 package org.randoom.setlx.statements;
 
 import org.randoom.setlx.exceptions.SetlException;
-import org.randoom.setlx.exceptions.SetlException;
 import org.randoom.setlx.exceptions.TermConversionException;
 import org.randoom.setlx.types.Term;
 import org.randoom.setlx.types.Value;

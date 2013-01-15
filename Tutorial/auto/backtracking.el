f89ea79b3aba02abf4800b0ceb890240b3ee4385
Herrmann, Tom <linux@randoom.org>
2013-01-15 21:25:04 +0100

    exclude auto completely

--- Tutorial/auto/backtracking.el
+++
@@ -1,8 +0,0 @@
-(TeX-add-style-hook "backtracking"
- (lambda ()
-    (LaTeX-add-labels
-     "fig:bisection-exception.stlx"
-     "fig:queens-nice.stlx"
-     "fig:queens-solution"
-     "fig:queens-all.stlx")))
-

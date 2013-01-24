a80d206418de8b946e7f647a5326f53bee0097f1
Herrmann, Tom <linux@randoom.org>
2013-01-24 16:25:11 +0100

    write string as single items, not as collection

--- src/java-src/org/randoom/setlx/functions/PD_writeFile.java
+++ src/java-src/org/randoom/setlx/functions/PD_writeFile.java
@@ -45,7 +45,7 @@ public class PD_writeFile extends PreDefinedFunction {
         final String          fileName    = fileArg.getUnquotedString();
         // get content to be written into the file
               CollectionValue content     = null;
-        if (contentArg instanceof CollectionValue && ! (contentArg instanceof Term)) {
+        if (contentArg instanceof CollectionValue && ! (contentArg instanceof Term || contentArg instanceof String)) {
             content = (CollectionValue) contentArg;
         } else {
             content = new SetlList(1);

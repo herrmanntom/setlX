7c58831944d8fc249edcbe301261d28155a16f80
Herrmann, Tom <linux@randoom.org>
2013-02-09 20:35:45 +0100

    added function nCPUs()

---
+++ src/java-src/org/randoom/setlx/functions/PD_nCPUs.java
@@ -0,0 +1,23 @@
+package org.randoom.setlx.functions;
+
+import org.randoom.setlx.types.Rational;
+import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.State;
+
+import java.util.List;
+
+// nCPUs()                       : get number of CPUs in current system
+
+public class PD_nCPUs extends PreDefinedFunction {
+    public final static PreDefinedFunction DEFINITION = new PD_nCPUs();
+
+    private PD_nCPUs() {
+        super("nCPUs");
+    }
+
+    @Override
+    public Value execute(final State state, final List<Value> args, final List<Value> writeBackVars) {
+        return Rational.valueOf(state.getNumberOfCores());
+    }
+}
+

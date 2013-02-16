bd490aa40cb5b25cd56f033ebf0d733b9b2e5d1e
Herrmann, Tom <linux@randoom.org>
2013-02-16 17:45:33 +0100

    fix bug when updating member variables

--- src/java-src/org/randoom/setlx/utilities/VariableScope.java
+++ src/java-src/org/randoom/setlx/utilities/VariableScope.java
@@ -167,11 +167,11 @@ public class VariableScope {
     }
 
     /*package*/ void storeValue(final String var, final Value value) {
-        if ( ! mWriteThrough || mVarBindings.get(var) != null) {
+        if ( ! mWriteThrough && mObjectScope != null && mObjectScope.isObjectMemberDefinied(var)) {
+            mObjectScope.setObjectMember(var, value);
+        } else if ( ! mWriteThrough || mVarBindings.get(var) != null) {
             // this scope does not allow write through or variable is actually stored here
             mVarBindings.put(var, value);
-        } else if ( ! mWriteThrough && mObjectScope != null && mObjectScope.isObjectMemberDefinied(var)) {
-            mObjectScope.setObjectMember(var, value);
         } else if (mWriteThrough          && // allowed to write into mOriginalScope
                    mOriginalScope != null && // mOriginalScope exists
                    ( ! mRestrictToFunctions || value instanceof ProcedureDefinition) // not restricted

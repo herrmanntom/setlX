e93a28f53bd5945d845b9c773616557db973f86b
autarch princeps <autarch@outlook.com>
2014-02-03 16:56:56 +0100

    Prj

---
+++ src/java-src/org/randoom/setlx/types/Matrix.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package org.randoom.setlx.types;
+
+import org.randoom.setlx.utilities.State;
+
+/**
+ * @author Patrick Robinson
+ *
+ */
+public class Matrix extends Value {
+
+	/* (non-Javadoc)
+	 * @see org.randoom.setlx.types.Value#clone()
+	 */
+	@Override
+	public Value clone() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.randoom.setlx.types.Value#appendString(org.randoom.setlx.utilities.State, java.lang.StringBuilder, int)
+	 */
+	@Override
+	public void appendString(State state, StringBuilder sb, int tabs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.randoom.setlx.types.Value#compareTo(org.randoom.setlx.types.Value)
+	 */
+	@Override
+	public int compareTo(Value other) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.randoom.setlx.types.Value#compareToOrdering()
+	 */
+	@Override
+	protected int compareToOrdering() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.randoom.setlx.types.Value#equalTo(org.randoom.setlx.types.Value)
+	 */
+	@Override
+	public boolean equalTo(Value other) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.randoom.setlx.types.Value#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}

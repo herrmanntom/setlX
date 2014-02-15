2e3a73682f3c0dadc6f4e15e19976ce9dbd527d2
autarch princeps <autarch@outlook.com>
2014-02-15 19:35:25 +0100

    matrix function parameter

--- src/java-src/org/randoom/setlx/functions/PD_matrix.java
+++ src/java-src/org/randoom/setlx/functions/PD_matrix.java
@@ -9,22 +9,26 @@ import org.randoom.setlx.exceptions.SetlException;
 import org.randoom.setlx.types.CollectionValue;
 import org.randoom.setlx.types.Matrix;
 import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.ParameterDef;
 import org.randoom.setlx.utilities.State;
 
 /**
- * @author autarch
+ * @author Patrick Robinson
  *
  */
 public class PD_matrix extends PreDefinedProcedure {
-
-	/* (non-Javadoc)
-	 * @see org.randoom.setlx.functions.PreDefinedProcedure#execute(org.randoom.setlx.utilities.State, java.util.List, java.util.List)
-	 */
-	@Override
-	protected Value execute(State state, List<Value> args,
-			List<Value> writeBackVars) throws SetlException {
-		
-		return new Matrix((CollectionValue)args.get(0));
-	}
-
+    public final static PreDefinedProcedure DEFINITION = new PD_matrix();
+    
+    private PD_matrix() {
+        super();
+        addParameter("collectionValue", ParameterDef.READ_ONLY);
+    }
+    
+    /* (non-Javadoc)
+     * @see org.randoom.setlx.functions.PreDefinedProcedure#execute(org.randoom.setlx.utilities.State, java.util.List, java.util.List)
+     */
+    @Override
+    public Value execute(State state, List<Value> args, List<Value> writeBackVars) throws SetlException {
+            return new Matrix((CollectionValue)args.get(0));
+    }
 }

b2206ff82e68efa9a2a599e24c9fdac556091890
autarch princeps <autarch@outlook.com>
2014-02-19 16:29:10 +0100

    Singular Value Decomposition

---
+++ src/java-src/org/randoom/setlx/functions/PD_singularValueDecomposition.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.randoom.setlx.functions;
+
+import Jama.SingularValueDecomposition;
+import java.util.List;
+import org.randoom.setlx.exceptions.MatrixException;
+import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.types.Matrix;
+import org.randoom.setlx.types.SetlList;
+import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.ParameterDef;
+import org.randoom.setlx.utilities.State;
+
+/**
+ *
+ * @author autarch
+ */
+public class PD_singularValueDecomposition extends PreDefinedProcedure {
+    public final static PreDefinedProcedure DEFINITION = new PD_singularValueDecomposition();
+    
+    private PD_singularValueDecomposition() {
+        super();
+        addParameter("Matrix", ParameterDef.READ_ONLY);
+    }    
+
+    @Override
+    protected Value execute(State state, List<Value> args, List<Value> writeBackVars) throws SetlException {
+        if(!(args.get(0) instanceof Matrix)) throw new MatrixException("The Parameter needs to be a Matrix.");
+        // TODO check condition
+        SingularValueDecomposition result = ((Matrix)args.get(0)).value.svd();
+        SetlList container = new SetlList(3);
+        container.addMember(state, new Matrix(result.getU())); // TODO right format?
+        container.addMember(state, new Matrix(result.getS())); // TODO Is this Σ? format?
+        container.addMember(state, new Matrix(result.getV())); // TODO right format?
+        return container;
+    }
+}

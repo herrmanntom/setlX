84df4ca8bc321075705b177b60e2b57e7fc88e57
autarch princeps <autarch@outlook.com>
2014-02-19 18:00:39 +0100

    determinante

---
+++ src/java-src/org/randoom/setlx/functions/PD_matrixdeterminant.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package org.randoom.setlx.functions;
+
+import java.util.List;
+import org.randoom.setlx.exceptions.MatrixException;
+import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.types.Matrix;
+import org.randoom.setlx.types.SetlDouble;
+import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.ParameterDef;
+import org.randoom.setlx.utilities.State;
+
+/**
+ *
+ * @author autarch
+ */
+public class PD_matrixdeterminant extends PreDefinedProcedure {
+    public final static PreDefinedProcedure DEFINITION = new PD_matrixdeterminant();
+    
+    private PD_matrixdeterminant() {
+        super();
+        addParameter("Matrix", ParameterDef.READ_ONLY);
+    }
+
+    @Override
+    protected Value execute(State state, List<Value> args, List<Value> writeBackVars) throws SetlException {
+        if(!(args.get(0) instanceof Matrix)) throw new MatrixException("The parameter needs to be a matrix.");
+        // TODO are there conditions for determinant?
+        return new SetlDouble(((Matrix)args.get(0)).value.det());
+    }
+}

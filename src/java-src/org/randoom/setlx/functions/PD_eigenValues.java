6a03836f1f5885da683788abaf01850f38a54fe3
autarch princeps <autarch@outlook.com>
2014-02-26 16:08:54 +0100

    eingenValues SetlList return type

--- src/java-src/org/randoom/setlx/functions/PD_eigenValues.java
+++ src/java-src/org/randoom/setlx/functions/PD_eigenValues.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.randoom.setlx.exceptions.MatrixException;
 import org.randoom.setlx.exceptions.SetlException;
 import org.randoom.setlx.types.Matrix;
+import org.randoom.setlx.types.SetlDouble;
+import org.randoom.setlx.types.SetlList;
 import org.randoom.setlx.types.Value;
 import org.randoom.setlx.utilities.ParameterDef;
 import org.randoom.setlx.utilities.State;
@@ -25,6 +27,12 @@ public class PD_eigenValues extends PreDefinedProcedure {
         if(!(args.get(0) instanceof Matrix)) throw new MatrixException("The parameter needs to be a matrix.");
         // TODO check condition
         EigenvalueDecomposition result = ((Matrix)args.get(0)).value.eig();
-        return new Matrix(result.getD()); // TODO right result?
+        // return new Matrix(result.getD()); // TODO right result?
+        double[][] values = result.getD().getArray();
+        SetlList composition = new SetlList(values.length);
+        for(int i = 0; i < values.length; i++) {
+            composition.addMember(state, SetlDouble.valueOf(values[i][i]));
+        }
+        return composition;
     }
 }

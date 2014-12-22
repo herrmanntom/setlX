cd76ebcb6c533dce4f9cbd7c725c4685ecc79680
Herrmann, Tom <setlX@randoom.org>
2014-12-22 16:27:43 +0100

    Improve JUnit test runner to load all classes from given path

--- interpreter/core/src/test/java/org/randoom/setlx/TestRunner.java
+++ interpreter/core/src/test/java/org/randoom/setlx/TestRunner.java
@@ -3,32 +3,97 @@ package org.randoom.setlx;
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
-import org.randoom.setlx.types.*;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 
 /**
  * Run all tests.
  */
 public class TestRunner {
     /**
-     * Main function which runs all tests
+     * Main function which runs all tests in source files ending with Test.java
      *
-     * @param args ignored
+     * @param args path to directory that contains source of test classes
      */
     public static void main(String[] args) {
-        System.out.println("Running setlX unit tests:");
-        Result testResult = JUnitCore.runClasses(
-                SetlListTest.class,
-                SetlMatrixTest.class,
-                SetlSetTest.class,
-                SetlStringTest.class,
-                SetlVectorTest.class
-        );
-        for (Failure f : testResult.getFailures()) {
-            System.err.println("Failure: " + f.toString());
-            // System.err.println(f.getMessage() + " " + f.getTrace() + " " + f.getDescription() + " " + f.getException());
+        if (args.length != 1) {
+            System.err.println("Parameter missing! Please pass directory that contains sources of test classes as only parameter.");
+            System.exit(1);
         }
-        System.out.println("Tests run: " + testResult.getRunCount() + ", Tests failures: " + testResult.getFailureCount());
-        if (testResult.getFailureCount() > 0) {
+        File testDir = new File(args[0]);
+        if (!testDir.isDirectory() || ! testDir.canRead()) {
+            System.err.println("Parameter is not a valid directory.");
+            System.exit(1);
+        }
+        ArrayList<Class<?>> testClasses    = new ArrayList<Class<?>>();
+        LinkedList<File>    directoryQueue = new LinkedList<File>();
+        directoryQueue.add(testDir);
+        while (!directoryQueue.isEmpty()) {
+            File directory = directoryQueue.poll();
+            File[] files = directory.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    if (file.isDirectory() && file.canRead()) {
+                        directoryQueue.add(file);
+                    } else if (file.isFile() && file.canRead()) {
+                        String fileName = file.getName();
+                        if (fileName.endsWith("Test.java")) {
+                            try {
+                                BufferedReader br = new BufferedReader(new FileReader(file));
+                                try {
+                                    String line;
+                                    do {
+                                        line = br.readLine();
+                                        if (line != null && line.startsWith("package ") && line.endsWith(";")) {
+                                            String packageName = line.replace("package ","").replace(";","");
+                                            testClasses.add(Class.forName(packageName + "." + fileName.replace(".java", "")));
+                                        }
+                                    } while (line != null);
+                                } finally {
+                                    br.close();
+                                }
+                            } catch (ClassNotFoundException e) {
+                                e.printStackTrace();
+                                System.exit(1);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                                System.exit(1);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (testClasses.isEmpty()) {
+            System.err.println("No test classes found in " + testDir.getAbsolutePath() + ".");
+            System.exit(1);
+        }
+
+        System.out.println("Running setlX unit tests...");
+        int testsRun = 0;
+        int failures = 0;
+        for (Class<?> testClass : testClasses) {
+            System.out.println("  " + testClass.getSimpleName() + ":");
+            Result testResult = JUnitCore.runClasses(testClass);
+            for (Failure f : testResult.getFailures()) {
+                System.err.println("    Failure: " + f.getMessage());
+                f.getException().printStackTrace();
+            }
+            System.out.println("    Tests run: " + testResult.getRunCount() + ", Tests failures: " + testResult.getFailureCount());
+            testsRun += testResult.getRunCount();
+            failures += testResult.getFailureCount();
+        }
+
+        System.out.println("  -----------------------------------");
+        System.out.println("    Tests run: " + testsRun + ", Tests failures: " + failures);
+        System.out.println("  ===================================");
+
+        if (failures > 0) {
             System.err.println("Still buggy");
             System.exit(1);
         } else {

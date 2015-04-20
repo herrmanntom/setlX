2f1f3bd6d0e703cb50c3a29ab1ef3c648f96c9b9
fabian <topfdrummer@googlemail.com>
2015-04-20 21:20:14 +0200

    added PD_addChart

---
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_addChart.java
@@ -0,0 +1,30 @@
+package org.randoom.setlx.functions;
+
+import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.types.Rational;
+import org.randoom.setlx.types.SetlString;
+import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.ParameterDef;
+import org.randoom.setlx.utilities.State;
+
+import java.util.HashMap;
+
+public class PD_addChart extends PreDefinedProcedure {
+
+    private final static ParameterDef CANVAS = createParameter("canvas");
+    private final static ParameterDef CHARTTYPE = createParameter("chartType");
+    private final static ParameterDef VALUES = createParameter("values");
+    private final static ParameterDef NAME = createOptionalParameter("name", Rational.ONE);
+    public final static PreDefinedProcedure DEFINITION = new PD_addChart();
+    private PD_addChart(){
+        super();
+        addParameter(CANVAS);
+        addParameter(CHARTTYPE);
+        addParameter(VALUES);
+        addParameter(NAME);
+    }
+    @Override
+    protected Value execute(State state, HashMap<ParameterDef, Value> args) throws SetlException {
+        return new SetlString(String.valueOf(args.entrySet()));
+    }
+}

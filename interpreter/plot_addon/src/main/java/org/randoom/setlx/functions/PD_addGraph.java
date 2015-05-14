274384ae16b901c1fae680acd8a83a8837919904
topfdrummer <topfdrummer@googlemail.com>
2015-05-14 15:25:59 +0200

    commented and fixed wrong function call

--- interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_addGraph.java
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_addGraph.java
@@ -5,24 +5,24 @@ import org.randoom.setlx.types.Rational;
 import org.randoom.setlx.types.SetlBoolean;
 import org.randoom.setlx.types.SetlString;
 import org.randoom.setlx.types.Value;
-import org.randoom.setlx.utilities.*;
+import org.randoom.setlx.utilities.Canvas;
+import org.randoom.setlx.utilities.ConnectJMathPlot;
+import org.randoom.setlx.utilities.ParameterDef;
+import org.randoom.setlx.utilities.State;
 
 import java.util.HashMap;
 
 public class PD_addGraph extends PreDefinedProcedure {
 
+
     private final static ParameterDef
             CANVAS = createParameter(" canvas ");
-
     private final static ParameterDef
             FUNCTIONDEFINITION = createParameter(" functiondefinition ");
-
     private final static ParameterDef
             GRAPHNAME = createOptionalParameter(" graphname ", Rational.ONE);
-
     private final static ParameterDef
             PLOTAREA = createOptionalParameter(" plotarea ", Rational.ONE);
-
     public final static PreDefinedProcedure
             DEFINITION = new PD_addGraph();
 
@@ -36,41 +36,48 @@ public class PD_addGraph extends PreDefinedProcedure {
 
     @Override
     protected Value execute(State state, HashMap<ParameterDef, Value> args) throws SetlException {
-        Canvas canvas = (Canvas)args.get(CANVAS);
-        SetlString functionDefinition = (SetlString)args.get(FUNCTIONDEFINITION);
-        String function = functionDefinition.toString();
-        Value graphname = args.get(GRAPHNAME);
+        // addGraph(Canvas canvas, String: function, [String: functionName], [Boolean: plotArea] )
+
+        // initialise parameter canvas, function, functionName and plotArea
+        Canvas canvas = (Canvas) args.get(CANVAS);
+        SetlString functionDefinition = (SetlString) args.get(FUNCTIONDEFINITION);
+        // function comes as ""sin(x)"" so i have to replace the quotation marks
+        String function = functionDefinition.toString().replace("\"", "");
+        Value functionName = args.get(GRAPHNAME);
         Value plotarea = args.get(PLOTAREA);
-        Graph g;
 
-        if(!graphname.equalTo(Rational.ONE) && !plotarea.equalTo(Rational.ONE)){
-            SetlString graphNameString = (SetlString)graphname;
-            SetlBoolean plotAreaBool = (SetlBoolean)plotarea;
+        // if functionName and plotArea are set
+        if (!functionName.equalTo(Rational.ONE) && !plotarea.equalTo(Rational.ONE)) {
+            SetlString graphNameString = (SetlString) functionName;
+            SetlBoolean plotAreaBool = (SetlBoolean) plotarea;
             boolean area;
-            if(plotAreaBool.equalTo(SetlBoolean.TRUE)){
+            if (plotAreaBool.equalTo(SetlBoolean.TRUE)) {
                 area = true;
-            }
-            else{
+            } else {
                 area = false;
             }
-            return ConnectJMathPlot.getInstance().addGraph(canvas, function,graphNameString.toString(),area );
-        }
-        if(!graphname.equalTo(Rational.ONE)){
-            SetlString graphNameString = (SetlString)graphname;
-            return ConnectJMathPlot.getInstance().addGraph(canvas, function,graphNameString.toString() );
-        }
-        if(!plotarea.equalTo(Rational.ONE)){
-            SetlBoolean plotAreaBool = (SetlBoolean)plotarea;
-            boolean area;
-            if(plotAreaBool.equalTo(SetlBoolean.TRUE)){
-                area = true;
-            }
-            else{
-                area = false;
-            }
-            return ConnectJMathPlot.getInstance().addGraph(canvas, function,area );
+            return ConnectJMathPlot.getInstance().addGraph(canvas, function, graphNameString.toString(), area);
         }
 
-        return ConnectJMathPlot.getInstance().addGraph(canvas, functionDefinition.toString());
+        //if only the functionName is set
+        if (!functionName.equalTo(Rational.ONE)) {
+            SetlString graphNameString = (SetlString) functionName;
+            return ConnectJMathPlot.getInstance().addGraph(canvas, function, graphNameString.toString());
+        }
+
+        //if only the plotarea is set
+        if (!plotarea.equalTo(Rational.ONE)) {
+            SetlBoolean plotAreaBool = (SetlBoolean) plotarea;
+            boolean area;
+            if (plotAreaBool.equalTo(SetlBoolean.TRUE)) {
+                area = true;
+            } else {
+                area = false;
+            }
+            return ConnectJMathPlot.getInstance().addGraph(canvas, function, area);
+        }
+
+        //if no optional parameter is set
+        return ConnectJMathPlot.getInstance().addGraph(canvas, function);
     }
 }

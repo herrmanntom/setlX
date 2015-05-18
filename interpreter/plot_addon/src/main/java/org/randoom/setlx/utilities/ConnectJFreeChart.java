605ea2fa3aa7b2924b342423b00c87e91d0d6ce4
Seeron <uebungsplatz@web.de>
2015-05-18 16:33:40 +0200

    implemented all plot functions. Added inner class to drawframe.

--- interpreter/plot_addon/src/main/java/org/randoom/setlx/utilities/ConnectJFreeChart.java
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/utilities/ConnectJFreeChart.java
@@ -2,6 +2,7 @@ package org.randoom.setlx.utilities;
 
 
 import org.jfree.data.xy.XYSeries;
+import org.jfree.chart.axis.*;
 
 import java.util.List;
 
@@ -19,73 +20,72 @@ public class ConnectJFreeChart implements SetlXPlot {
 
     @Override
     public Graph addGraph(Canvas canvas, String function) {
-        XYSeries data = canvas.getFrame().addDataset("function", function);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addDataset("function", function, false);
         return new Graph(data);
     }
 
     @Override
     public Graph addGraph(Canvas canvas, String function, String name) {
-        XYSeries data = canvas.getFrame().addDataset(name, function);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addDataset(name, function, false);
+
         return new Graph(data);
     }
 
     //TODO: at plotArea possibility
     @Override
     public Graph addGraph(Canvas canvas, String function, boolean plotArea) {
-        XYSeries data = canvas.getFrame().addDataset("function", function);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addDataset("function", function, plotArea);
+
         return new Graph(data);
     }
 
     @Override
     public Graph addGraph(Canvas canvas, String function, String name, boolean plotArea) {
-        XYSeries data = canvas.getFrame().addDataset(name, function);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addDataset(name, function, plotArea);
+
         return new Graph(data);
     }
 
     @Override
     public Graph addListGraph(Canvas canvas, List<List<Double>> function) {
-        XYSeries data = canvas.getFrame().addListDataset("function", function);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addListDataset("function", function, false);
+
         return new Graph(data);
     }
 
     @Override
     public Graph addListGraph(Canvas canvas, List<List<Double>> function, String name) {
-        XYSeries data = canvas.getFrame().addListDataset(name, function);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addListDataset(name, function, false);
+
         return new Graph(data);
     }
 
     @Override
     public Graph addParamGraph(Canvas canvas, String xfunction, String yfunction) {
-        XYSeries data = canvas.getFrame().addParamDataset("function",xfunction,yfunction);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addParamDataset("function", xfunction, yfunction, false);
+
         return new Graph(data);
     }
 
     @Override
     public Graph addParamGraph(Canvas canvas, String xfunction, String yfunction, String name) {
-        XYSeries data = canvas.getFrame().addParamDataset(name,xfunction,yfunction);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addParamDataset(name,xfunction,yfunction, false);
+
         return new Graph(data);
     }
 
     //TODO:plotArea
     @Override
     public Graph addParamGraph(Canvas canvas, String xfunction, String yfunction, Boolean plotArea) {
-        XYSeries data = canvas.getFrame().addParamDataset("function",xfunction,yfunction);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addParamDataset("function",xfunction,yfunction, plotArea);
+
         return new Graph(data);
     }
 
     @Override
     public Graph addParamGraph(Canvas canvas, String xfunction, String yfunction, String name, Boolean plotArea) {
-        XYSeries data = canvas.getFrame().addParamDataset(name,xfunction,yfunction);
-        canvas.getFrame().redraw();
+        XYSeries data = canvas.getFrame().addParamDataset(name,xfunction,yfunction, plotArea);
+
         return new Graph(data);
     }
 
@@ -122,7 +122,8 @@ public class ConnectJFreeChart implements SetlXPlot {
 
     @Override
     public void modScale(Canvas canvas, double xMin, double xMax, double yMin, double yMax) {
-
+        canvas.getFrame().modyScale(yMax, yMin);
+        canvas.getFrame().modxScale(xMax, xMin);
     }
 
     @Override
@@ -132,6 +133,25 @@ public class ConnectJFreeChart implements SetlXPlot {
 
     @Override
     public void modScaleType(Canvas canvas, String xType, String yType) {
+        System.out.println(xType+" "+yType);
+        if(xType.equalsIgnoreCase("\"log\"")){
+            canvas.getFrame().setxAxis(new LogAxis());
+        }
+        else if(xType.equalsIgnoreCase("\"num\"")){
+            canvas.getFrame().setxAxis(new NumberAxis());
+        }
+        else{
+            System.out.println("Wrong x-Axis type, use log or num");
+        }
+        if(yType.equalsIgnoreCase("\"log\"")){
+            canvas.getFrame().setyAxis(new LogAxis());
+        }
+        else if(yType.equalsIgnoreCase("\"num\"")){
+            canvas.getFrame().setyAxis(new NumberAxis());
+        }
+        else{
+            System.out.println("Wrong y-Axis type, use log or num");
+        }
 
     }
 
--- interpreter/plot_addon/src/main/java/org/randoom/setlx/utilities/DrawFrame.java
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/utilities/DrawFrame.java
@@ -1,13 +1,19 @@
 package org.randoom.setlx.utilities;
 
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.axis.ValueAxis;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYDifferenceRenderer;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
+import org.jfree.chart.renderer.xy.XYLineAndShapeRenderer;
 import org.jfree.data.xy.XYSeries;
 import org.jfree.data.xy.XYSeriesCollection;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -15,75 +21,236 @@ public class DrawFrame extends JFrame {
     private XYSeriesCollection dataset = new XYSeriesCollection();
     private double x_Min;
     private double x_Max;
-    private String xAxis;
-    private String yAxis;
-    private String chartTitle;
-    private JPanel jPanel;
-    //private ChartPanel chartPanel = null;
+    private double y_Min;
+    private double y_Max;
 
+    private List<Plotfunction> functions = new ArrayList<>();
+    private ValueAxis xAxis;
+    private ValueAxis yAxis;
+    private JPanel jPanel;
+    private XYPlot plot;
+    private int chartCount;
+    private JFreeChart chart;
+    private ChartPanel chartPanel;
+
+    public void setxAxis(ValueAxis xAxis) {
+        this.xAxis = xAxis;
+        if(plot !=null){
+            plot.setDomainAxis(this.xAxis);
+        }
+    }
+
+    public void modxScale(double x_Min, double x_Max){
+        this.x_Max = x_Max;
+        this.x_Min = x_Min;
+        ValueAxis axis = plot.getDomainAxis();
+        axis.setLowerBound(this.x_Min);
+        axis.setUpperBound(this.x_Max);
+        if(chartCount != 0) {
+            chartCount = 0;
+            jPanel.remove(chartPanel);
+            List<Plotfunction> func = new ArrayList<>(functions);
+            functions.clear();
+            for(Plotfunction item:func){
+                if(!item.getFunctionstring().isEmpty()){
+                    this.addDataset(item.getTitle(), item.getFunctionstring(), item.isArea());
+                }
+                else if(!item.getXfunction().isEmpty()){
+                    this.addParamDataset(item.getTitle(), item.getXfunction(), item.getYfunction(), item.isArea());
+                }
+                else if(item.getFunction() != null){
+                    this.addListDataset(item.getTitle(), item.getFunction(), item.isArea());
+                }
+            }
+        }
+    }
+    public void modyScale(double y_Min, double y_Max){
+        this.y_Max = y_Max;
+        this.y_Min = y_Min;
+        ValueAxis axis = plot.getRangeAxis();
+        axis.setLowerBound(this.y_Min);
+        axis.setUpperBound(this.y_Max);
+    }
+
+    public void setyAxis(ValueAxis yAxis) {
+        this.yAxis = yAxis;
+        if(plot !=null){
+            plot.setRangeAxis(this.yAxis);
+        }
+    }
     public DrawFrame() {
         super("Graphic output");
-
+        chartCount = 0;
         jPanel = new JPanel();
         add(jPanel, BorderLayout.CENTER);
-
         setSize(640, 480);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
-        xAxis = "X";
-        yAxis = "Y";
-        chartTitle = "Chart Title";
         x_Min = -10.0;
         x_Max = 10.0;
+        xAxis = new NumberAxis("x");
+        yAxis = new NumberAxis("y");
+        plot = null;
     }
 
-    public XYSeries addDataset(String title, String function ) {
+    public XYSeries addDataset(String title, String function, boolean area) {
+        Plotfunction plotfun = new Plotfunction(title, area);
+        plotfun.setFunctionstring(function);
+        functions.add(plotfun);
         XYSeries series = new XYSeries(title, true, false);
         CalcFunction calc = new CalcFunction(function);
-
+        XYItemRenderer renderer;
+        if(area){
+            renderer = new XYDifferenceRenderer();
+        }
+        else{
+            renderer = new XYLineAndShapeRenderer(true, false);
+        }
         double x = x_Min;
         while(x<=x_Max){
             series.add(x,calc.calcYfromX(x));
             x += 0.1;
         }
-        dataset.addSeries(series);
+        XYSeriesCollection col = new XYSeriesCollection(series);
 
+        if(plot == null){
+            plot = new XYPlot(col, xAxis, yAxis, renderer);
+        }
+        else{
+            plot.setDataset(chartCount, col);
+            plot.setRenderer(chartCount, renderer);
+        }
+        this.redraw();
         return series;
     }
 
-    public void redraw(){
-        JFreeChart chart = ChartFactory.createXYLineChart(chartTitle, xAxis, yAxis, this.dataset);
-        ChartPanel chartPanel = new ChartPanel(chart);
+    private void redraw(){
+        if(chartCount != 0) {
+            jPanel.remove(chartPanel);
+        }
+        chart = new JFreeChart("title", JFreeChart.DEFAULT_TITLE_FONT, plot, true);
+        chartPanel = new ChartPanel(chart, true, true, true, true, true);
+
         jPanel.add(chartPanel);
 
-        //this.pack();
-        this.setVisible(true);
-
-        /*
-        if(chartPanel == null) {
-            chartPanel = new ChartPanel(chart);
-            jPanel.add(chartPanel, BorderLayout.CENTER);
-            jPanel.validate();
-        }
-        */
+        this.pack();
+        chartCount++;
     }
-    public XYSeries addListDataset(String title, List<List<Double>> function){
+    public XYSeries addListDataset(String title, List<List<Double>> function, boolean area){
+        Plotfunction plotfun = new Plotfunction(title, area);
+        plotfun.setFunction(function);
+        functions.add(plotfun);
         XYSeries series = new XYSeries(title, false, true);
+        XYItemRenderer renderer;
+        if(area){
+            renderer = new XYDifferenceRenderer();
+        }
+        else{
+            renderer = new XYLineAndShapeRenderer(true, false);
+        }
         for(List<Double> element: function){
             series.add(element.get(0), element.get(1));
         }
-        dataset.addSeries(series);
+        XYSeriesCollection col = new XYSeriesCollection(series);
+
+        if(plot == null){
+            plot = new XYPlot(col, xAxis, yAxis, renderer);
+        }
+        else{
+            plot.setDataset(chartCount, col);
+            plot.setRenderer(chartCount, renderer);
+        }
+        this.redraw();
         return series;
     }
 
-    public XYSeries addParamDataset(String title, String xfunction, String yfunction){
+    public XYSeries addParamDataset(String title, String xfunction, String yfunction, boolean area){
+        Plotfunction plotfun = new Plotfunction(title, area);
+        plotfun.setXfunction(xfunction);
+        plotfun.setYfunction(yfunction);
+        functions.add(plotfun);
         XYSeries series = new XYSeries(title, true, false);
         CalcFunction xcalc = new CalcFunction(xfunction);
         CalcFunction ycalc = new CalcFunction(yfunction);
+        XYItemRenderer renderer;
+        if(area){
+            renderer = new XYDifferenceRenderer();
+        }
+        else{
+            renderer = new XYLineAndShapeRenderer(true, false);
+        }
         for(double x=-50; x<=50;x+=0.1){
             series.add(xcalc.calcYfromX(x),ycalc.calcYfromX(x));
         }
-        dataset.addSeries(series);
+        XYSeriesCollection col = new XYSeriesCollection(series);
+
+        if(plot == null){
+            plot = new XYPlot(col, xAxis, yAxis, renderer);
+        }
+        else{
+            plot.setDataset(chartCount, col);
+            plot.setRenderer(chartCount, renderer);
+        }
+        this.redraw();
         return series;
+    }
+
+    private class Plotfunction{
+        public boolean isArea() {
+            return area;
+        }
+
+        private boolean area;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public Plotfunction(String title, boolean area) {
+            this.title = title;
+            this.area = area;
+        }
+
+        public String getXfunction() {
+            return xfunction;
+        }
+
+        public List<List<Double>> getFunction() {
+            return function;
+        }
+
+        public String getYfunction() {
+            return yfunction;
+        }
+
+        private String title;
+
+        private String functionstring = "";
+
+        public String getFunctionstring() {
+            return functionstring;
+        }
+
+        public void setFunctionstring(String functionstring) {
+            this.functionstring = functionstring;
+        }
+
+        public void setFunction(List<List<Double>> function) {
+            this.function = function;
+        }
+
+        public void setXfunction(String xfunction) {
+            this.xfunction = xfunction;
+        }
+
+        public void setYfunction(String yfunction) {
+            this.yfunction = yfunction;
+        }
+
+        private List<List<Double>> function = null;
+
+        private String xfunction = "";
+
+        private String yfunction = "";
     }
 }

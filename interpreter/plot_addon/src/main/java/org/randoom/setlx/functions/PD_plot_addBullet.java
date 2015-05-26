e835763b5eddb8c3996126ae7b2aa791f0a6bebe
topfdrummer <topfdrummer@googlemail.com>
2015-05-26 16:08:41 +0200

    addBullet - optionalParameter, halfway

--- interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_plot_addBullet.java
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_plot_addBullet.java
@@ -1,6 +1,7 @@
 package org.randoom.setlx.functions;
 
 import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.types.Rational;
 import org.randoom.setlx.types.SetlList;
 import org.randoom.setlx.types.SetlString;
 import org.randoom.setlx.types.Value;
@@ -14,7 +15,7 @@ public class PD_plot_addBullet extends PreDefinedProcedure {
 
     private final static ParameterDef CANVAS = createParameter("canvas");
     private final static ParameterDef XYTUPEL = createParameter("xyTupel");
-    private final static ParameterDef RGBLIST = createParameter("RGBList");
+    private final static ParameterDef RGBLIST = createOptionalParameter("RGBList", Rational.ONE);
     public final static PreDefinedProcedure DEFINITION = new PD_plot_addBullet();
 
     private PD_plot_addBullet() {
@@ -27,6 +28,11 @@ public class PD_plot_addBullet extends PreDefinedProcedure {
     @Override
     protected Value execute(State state, HashMap<ParameterDef, Value> args) throws SetlException {
         SetlList xylist = (SetlList) args.get(XYTUPEL);
+
+        if(!args.get(RGBLIST).equalTo(Rational.ONE)){
+
+        }
+
         SetlList rgblistSetl = (SetlList) args.get(RGBLIST);
 
         if(!(rgblistSetl.size()==3)){

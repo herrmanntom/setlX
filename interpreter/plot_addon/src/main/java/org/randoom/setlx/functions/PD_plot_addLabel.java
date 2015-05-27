b8d4be0d57772dec4f66e5667f4968a5da806965
fabian <topfdrummer@googlemail.com>
2015-05-27 15:58:28 +0200

    rendamed insertlabel to labelAxis and added addLabel function

---
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_plot_addLabel.java
@@ -0,0 +1,36 @@
+package org.randoom.setlx.functions;
+
+import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.types.SetlList;
+import org.randoom.setlx.types.SetlString;
+import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.*;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class PD_plot_addLabel extends PreDefinedProcedure {
+
+    private final static ParameterDef CANVAS = createParameter("canvas");
+    private final static ParameterDef XYTUPEL = createParameter("XYTupel");
+    private final static ParameterDef LABEL = createParameter("Label");
+    public final static PreDefinedProcedure DEFINITION = new PD_plot_addLabel();
+
+    private PD_plot_addLabel() {
+        super();
+        addParameter(CANVAS);
+        addParameter(XYTUPEL);
+        addParameter(LABEL);
+    }
+
+    @Override
+    protected Value execute(State state, HashMap<ParameterDef, Value> args) throws SetlException {
+        Canvas canvas = (Canvas)args.get(CANVAS);
+        SetlList xyTupel = (SetlList)args.get(XYTUPEL);
+        List list = ConvertSetlTypes.convertSetlListAsDouble(xyTupel);
+        SetlString label = (SetlString)args.get(LABEL);
+        String stringLabel = label.toString().replace("\"", "");
+
+        return ConnectJFreeChart.getInstance().addLabel(canvas, list, stringLabel);
+    }
+}

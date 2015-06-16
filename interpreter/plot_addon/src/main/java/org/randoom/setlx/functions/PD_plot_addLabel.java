89f05c9aba46461c3778f600b976cbb929a5c618
fabian <topfdrummer@googlemail.com>
2015-06-16 18:33:59 +0200

    added param. error handling to addLabel

--- interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_plot_addLabel.java
+++ interpreter/plot_addon/src/main/java/org/randoom/setlx/functions/PD_plot_addLabel.java
@@ -1,6 +1,8 @@
 package org.randoom.setlx.functions;
 
+import jdk.nashorn.internal.runtime.Undefined;
 import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.exceptions.UndefinedOperationException;
 import org.randoom.setlx.types.SetlList;
 import org.randoom.setlx.types.SetlString;
 import org.randoom.setlx.types.Value;
@@ -25,8 +27,30 @@ public class PD_plot_addLabel extends PreDefinedProcedure {
 
     @Override
     protected Value execute(State state, HashMap<ParameterDef, Value> args) throws SetlException {
+
+        if(!CheckType.isCanvas(args.get(CANVAS))){
+            throw new UndefinedOperationException("First parameter has to be a Canvas object");
+        }
+
+        if(!CheckType.isSetlList(args.get(XYTUPEL))){
+            throw new UndefinedOperationException("Second parameter has to be a SetlList (eq: [1,2]) ");
+        }
+
+        if(!CheckType.isSetlString(args.get(LABEL))){
+            throw new UndefinedOperationException("Third parameter hast do be a String (eq: \"Text on Label\")");
+        }
+
         Canvas canvas = (Canvas)args.get(CANVAS);
         SetlList xyTupel = (SetlList)args.get(XYTUPEL);
+
+        if(xyTupel.size() != 2){
+            throw new UndefinedOperationException("Second parameter has to be a Tupel of Numbers (eq: [1,2]) ");
+        }
+
+        if(!CheckType.isSetlListWithNumbers(xyTupel)){
+            throw new UndefinedOperationException("Second parameter has to be a Tupel of Numbers (eq: [1,2]) ");
+        }
+
         List<Double> list = ConvertSetlTypes.convertSetlListAsDouble(xyTupel);
         SetlString label = (SetlString)args.get(LABEL);
         String stringLabel = label.toString().replace("\"", "");

de4e78a6bccfb7e3214bdd82993ca7a284c9d4c9
Herrmann, Tom <setlX@randoom.org>
2015-12-19 10:52:47 +0100

    Add isVariable function

---
+++ interpreter/core/src/main/java/org/randoom/setlx/functions/PD_isVariable.java
@@ -0,0 +1,49 @@
+package org.randoom.setlx.functions;
+
+import org.randoom.setlx.exceptions.IncompatibleTypeException;
+import org.randoom.setlx.operators.Variable;
+import org.randoom.setlx.types.SetlBoolean;
+import org.randoom.setlx.types.Term;
+import org.randoom.setlx.types.Value;
+import org.randoom.setlx.utilities.ParameterDef;
+import org.randoom.setlx.utilities.State;
+
+import java.util.HashMap;
+
+/**
+ * isVariable(term) : Returns true if `integer' is a prime, false otherwise.
+ *                    Unlike isProbablePrime() this functions actually
+ *                    tests all possible factors, therefore has linear
+ *                    complexity.
+ */
+public class PD_isVariable extends PreDefinedProcedure {
+
+    private final static ParameterDef TERM = createParameter("term");
+
+    /** Definition of the PreDefinedProcedure `isVariable'. */
+    public  final static PreDefinedProcedure DEFINITION = new PD_isVariable();
+
+    private PD_isVariable() {
+        super();
+        addParameter(TERM);
+    }
+
+    @Override
+    public Value execute(final State state, final HashMap<ParameterDef, Value> args) throws IncompatibleTypeException {
+        final Value parameterValue  = args.get(TERM);
+        if (parameterValue.isTerm() == SetlBoolean.TRUE) {
+            Term term = (Term) parameterValue;
+            if (
+                    Variable.getFunctionalCharacterExternal().equals(term.getFunctionalCharacter())
+                    && term.firstMember().isString() == SetlBoolean.TRUE
+            ) {
+                return SetlBoolean.TRUE;
+            }
+        }
+
+        return SetlBoolean.FALSE;
+
+    }
+
+}
+

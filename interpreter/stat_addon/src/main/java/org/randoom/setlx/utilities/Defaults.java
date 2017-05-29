6ade694106e23f64f31e2515bdd3dadbca70ebdf
Jonas Siefker <jonas.siefker@tis.biz>
2017-05-29 11:07:02 +0200

    minor correction

--- interpreter/stat_addon/src/main/java/org/randoom/setlx/utilities/Defaults.java
+++ interpreter/stat_addon/src/main/java/org/randoom/setlx/utilities/Defaults.java
@@ -8,7 +8,6 @@ import org.randoom.setlx.types.SetlList;
 import org.randoom.setlx.types.Value;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -20,6 +19,15 @@ public class Defaults {
     /** Default plot interval of 0.01 **/
     public static SetlDouble getDefaultPlotInterval() {
         return createSetlDoubleValue(0.01);
+    }
+
+    /** Default graph color scheme (black) */
+    public static List<Integer> DEFAULT_COLOR_SCHEME;
+    static {
+        DEFAULT_COLOR_SCHEME = new ArrayList<>();
+        DEFAULT_COLOR_SCHEME.add(0);
+        DEFAULT_COLOR_SCHEME.add(0);
+        DEFAULT_COLOR_SCHEME.add(0);
     }
 
     public static SetlDouble createSetlDoubleValue(double value) {
@@ -35,7 +43,7 @@ public class Defaults {
         List<Integer> colorScheme = new ArrayList<>();
 
         if (color.isString() == SetlBoolean.TRUE && color.toString().equals("\"DEFAULT_COLOR\"")) {
-            return new ArrayList<>(Arrays.asList(0,0,0));
+            return DEFAULT_COLOR_SCHEME;
         } else {
             for (Iterator<Value> value = ((SetlList) color).iterator(); value.hasNext();) {
                 colorScheme.add(value.next().toJIntValue(state));

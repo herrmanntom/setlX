b265cdb0112a029f979a4b4ba6293f1f025e9a56
Tom Herrmann <setlX@randoom.org>
2019-06-05 09:47:30 +0200

    Improve xml parsing to handle child nodes and attributes

--- interpreter/webapi_addon/src/main/java/org/randoom/setlx/functions/PD_webapi_parse_xml.java
+++ interpreter/webapi_addon/src/main/java/org/randoom/setlx/functions/PD_webapi_parse_xml.java
@@ -1,31 +1,16 @@
 package org.randoom.setlx.functions;
 
 import org.randoom.setlx.exceptions.IncompatibleTypeException;
-import org.randoom.setlx.exceptions.JVMException;
 import org.randoom.setlx.exceptions.SetlException;
 import org.randoom.setlx.parameters.ParameterDefinition;
-import org.randoom.setlx.statements.Block;
 import org.randoom.setlx.types.SetlBoolean;
-import org.randoom.setlx.types.SetlClass;
-import org.randoom.setlx.types.SetlList;
-import org.randoom.setlx.types.SetlObject;
-import org.randoom.setlx.types.SetlString;
 import org.randoom.setlx.types.Value;
-import org.randoom.setlx.utilities.ParseSetlX;
 import org.randoom.setlx.utilities.State;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.xml.sax.SAXException;
+import org.randoom.setlx.utilities.XmlParser;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 
 /**
  * webapi_parse_xml(xml_string) : Use javax parsers to parse given xml content into document and map it to setlx objects.
@@ -33,13 +18,6 @@ import java.util.List;
 public class PD_webapi_parse_xml extends PreDefinedProcedure {
 
     private final static ParameterDefinition XML_STRING = createParameter("xml_string");
-
-    private final static String CLASS_CODE_NODE = "class webapi_xml_node(name, text_content, child_nodes) {\n" +
-                                                  "    this.name := name;\n" +
-                                                  "    this.text_content := text_content;\n" +
-                                                  "    this.child_nodes := child_nodes;\n" +
-                                                  "}";
-    private       static SetlClass xmlNodeClass = null;
 
     /** Definition of the PreDefinedProcedure 'stat_beta' */
     public final static PreDefinedProcedure DEFINITION = new PD_webapi_parse_xml();
@@ -53,45 +31,10 @@ public class PD_webapi_parse_xml extends PreDefinedProcedure {
     public Value execute(State state, HashMap<ParameterDefinition, Value> args) throws SetlException {
         final Value argument = args.get(XML_STRING);
         if (argument.isString() == SetlBoolean.FALSE || argument.size() == 0) {
-            throw new IncompatibleTypeException("Could not '" + argument.getUnquotedString(state) + "' as xml");
+            throw new IncompatibleTypeException("Could not parse '" + argument.getUnquotedString(state) + "' as xml");
         }
         String xmlString = argument.getUnquotedString(state);
 
-        Document xmlDocument;
-        try {
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            documentBuilderFactory.setIgnoringElementContentWhitespace(true);
-            DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
-            xmlDocument = builder.parse(new ByteArrayInputStream(xmlString.getBytes(StandardCharsets.UTF_8)));
-        } catch (ParserConfigurationException | SAXException | IOException e) {
-            throw new JVMException("Could not parse '" + argument.getUnquotedString(state) + "' as xml", e);
-        }
-
-        if (xmlNodeClass == null) {
-            Block block = ParseSetlX.parseStringToBlock(state, CLASS_CODE_NODE);
-            block.execute(state);
-            xmlNodeClass = (SetlClass) state.findValue("webapi_xml_node");
-        }
-
-
-        Element documentElement = xmlDocument.getDocumentElement();
-
-        SetlList childNodes = extractChildNodes(state, documentElement);
-
-        SetlObject rootNode = newXmlNode(state, documentElement.getNodeName(), documentElement.getTextContent(), childNodes);
-
-        return rootNode;
-    }
-
-    private SetlList extractChildNodes(State state, Element documentElement) {
-        return new SetlList();
-    }
-
-    private SetlObject newXmlNode(State state, String nodeName, String textContent, SetlList childNodes) throws SetlException {
-        List<Value> argumentValues = new ArrayList<>();
-        argumentValues.add(new SetlString(nodeName));
-        argumentValues.add(new SetlString(textContent));
-        argumentValues.add(childNodes);
-        return (SetlObject) xmlNodeClass.call(state, argumentValues, null, null, null);
+        return XmlParser.parse(state, new ByteArrayInputStream(xmlString.getBytes(StandardCharsets.UTF_8)));
     }
 }
---
+++ interpreter/webapi_addon/src/main/java/org/randoom/setlx/utilities/XmlParser.java
@@ -0,0 +1,102 @@
+package org.randoom.setlx.utilities;
+
+import org.randoom.setlx.exceptions.IncompatibleTypeException;
+import org.randoom.setlx.exceptions.JVMException;
+import org.randoom.setlx.exceptions.SetlException;
+import org.randoom.setlx.statements.Block;
+import org.randoom.setlx.types.Om;
+import org.randoom.setlx.types.SetlBoolean;
+import org.randoom.setlx.types.SetlClass;
+import org.randoom.setlx.types.SetlList;
+import org.randoom.setlx.types.SetlObject;
+import org.randoom.setlx.types.SetlSet;
+import org.randoom.setlx.types.SetlString;
+import org.randoom.setlx.types.Value;
+import org.w3c.dom.Document;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class XmlParser {
+
+    private static final String CLASS_NAME = "webapi_xml_node";
+
+    private final static String CLASS_CODE_NODE = "class " + CLASS_NAME + "(node_name, attributes, text_content, child_nodes) {\n" +
+                                                  "    this.node_name := node_name;\n" +
+                                                  "    this.attributes := attributes;\n" +
+                                                  "    this.text_content := text_content;\n" +
+                                                  "    this.child_nodes := child_nodes;\n" +
+                                                  "}";
+
+    public static SetlObject parse(State state, InputStream inputStream) throws SetlException {
+        Value classCandidate = state.findValue(CLASS_NAME);
+        if (classCandidate == Om.OM) {
+            Block block = ParseSetlX.parseStringToBlock(state, CLASS_CODE_NODE);
+            block.execute(state);
+            classCandidate = state.findValue(CLASS_NAME);
+        }
+
+        if (classCandidate.isClass() == SetlBoolean.FALSE) {
+            throw new IncompatibleTypeException("Value bound to variable '" + CLASS_NAME + "' is not a class: " + classCandidate.toString(state));
+        }
+
+        Document xmlDocument;
+        try {
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            documentBuilderFactory.setIgnoringElementContentWhitespace(true);
+            DocumentBuilder builder = documentBuilderFactory.newDocumentBuilder();
+            xmlDocument = builder.parse(inputStream);
+        } catch (ParserConfigurationException | SAXException | IOException e) {
+            throw new JVMException("Could not parse xml", e);
+        }
+
+        return mapXmlNode(state, (SetlClass) classCandidate, xmlDocument.getDocumentElement());
+    }
+
+    private static SetlObject mapXmlNode(State state, SetlClass xmlNodeClass, Node node) throws SetlException {
+        List<Value> argumentValues = new ArrayList<>();
+        argumentValues.add(new SetlString(node.getNodeName()));
+        argumentValues.add(mapAttributes(state, node));
+        argumentValues.add(new SetlString(node.getTextContent()));
+        argumentValues.add(mapChildNodes(state, xmlNodeClass, node));
+        return (SetlObject) xmlNodeClass.call(state, argumentValues, null, null, null);
+    }
+
+    private static SetlSet mapAttributes(State state, Node node) {
+        SetlSet attributes = new SetlSet();
+        NamedNodeMap attributeMap = node.getAttributes();
+        for (int i = 0; i < attributeMap.getLength(); ++i) {
+            Node attribute = attributeMap.item(i);
+
+            SetlList attributePair = new SetlList();
+            attributePair.addMember(state, new SetlString(attribute.getNodeName()));
+            attributePair.addMember(state, new SetlString(attribute.getNodeValue()));
+
+            attributes.addMember(state, attributePair);
+        }
+        return attributes;
+    }
+
+    private static SetlList mapChildNodes(State state, SetlClass xmlNodeClass, Node node) throws SetlException {
+        SetlList children = new SetlList();
+
+        NodeList childNodes = node.getChildNodes();
+        for (int i = 0; i < childNodes.getLength(); ++i) {
+            Node child = childNodes.item(i);
+            if (!"#text".equals(child.getNodeName())) {
+                children.addMember(state, mapXmlNode(state, xmlNodeClass, child));
+            }
+        }
+
+        return children;
+    }
+}

c6839dad53fd5aaa5a79d54695a3860629339c51
Tom Herrmann <setlX@randoom.org>
2019-06-06 12:43:14 +0200

    add GET request

--- interpreter/webapi_addon/src/main/java/org/randoom/setlx/functions/PD_webapi_get.java
+++ interpreter/webapi_addon/src/main/java/org/randoom/setlx/functions/PD_webapi_get.java
@@ -4,7 +4,6 @@ import org.randoom.setlx.exceptions.IncompatibleTypeException;
 import org.randoom.setlx.exceptions.SetlException;
 import org.randoom.setlx.parameters.ParameterDefinition;
 import org.randoom.setlx.types.SetlBoolean;
-import org.randoom.setlx.types.SetlList;
 import org.randoom.setlx.types.SetlSet;
 import org.randoom.setlx.types.Value;
 import org.randoom.setlx.utilities.State;
@@ -13,30 +12,30 @@ import org.randoom.setlx.utilities.WebRequests;
 import java.util.HashMap;
 
 /**
- * webapi_post(targetUrl, formDataMap, cookieDataMap) : Send POST request to target url
+ * webapi_get(targetUrl, cookieDataMap) : Send GET request to target url
  */
-public class PD_webapi_post extends PreDefinedProcedure {
+public class PD_webapi_get extends PreDefinedProcedure {
 
-    private final static ParameterDefinition TARGET_URL = createParameter("targetUrl");
-    private final static ParameterDefinition FORM_DATA_MAP = createParameter("formDataMap");
+    private final static ParameterDefinition BASE_URL = createParameter("baseUrl");
+    private final static ParameterDefinition QUERY_PARAMETER_MAP = createParameter("queryParameterMap");
     private final static ParameterDefinition COOKIE_DATA_MAP = createParameter("cookieDataMap");
 
-    public final static PreDefinedProcedure DEFINITION = new PD_webapi_post();
+    public final static PreDefinedProcedure DEFINITION = new PD_webapi_get();
 
-    private PD_webapi_post() {
+    private PD_webapi_get() {
         super();
-        addParameter(TARGET_URL);
-        addParameter(FORM_DATA_MAP);
+        addParameter(BASE_URL);
+        addParameter(QUERY_PARAMETER_MAP);
         addParameter(COOKIE_DATA_MAP);
     }
 
     @Override
     public Value execute(State state, HashMap<ParameterDefinition, Value> args) throws SetlException {
-        final Value argument = args.get(TARGET_URL);
+        final Value argument = args.get(BASE_URL);
         if (argument.isString() == SetlBoolean.FALSE || argument.size() == 0) {
             throw new IncompatibleTypeException("Parameter is not a string: " + argument.toString(state));
         }
-        final Value argument2 = args.get(FORM_DATA_MAP);
+        final Value argument2 = args.get(QUERY_PARAMETER_MAP);
         if (argument2.isMap() == SetlBoolean.FALSE) {
             throw new IncompatibleTypeException("Parameter is not a map: " + argument2.toString(state));
         }
@@ -46,6 +45,6 @@ public class PD_webapi_post extends PreDefinedProcedure {
         }
         String url = argument.getUnquotedString(state);
 
-        return WebRequests.post(state, url, (SetlSet) argument2, (SetlSet) argument3);
+        return WebRequests.get(state, url, (SetlSet) argument2, (SetlSet) argument3);
     }
 }
--- interpreter/webapi_addon/src/main/java/org/randoom/setlx/utilities/WebRequests.java
+++ interpreter/webapi_addon/src/main/java/org/randoom/setlx/utilities/WebRequests.java
@@ -6,7 +6,6 @@ import org.randoom.setlx.statements.Block;
 import org.randoom.setlx.types.Om;
 import org.randoom.setlx.types.Rational;
 import org.randoom.setlx.types.SetlBoolean;
-import org.randoom.setlx.types.SetlClass;
 import org.randoom.setlx.types.SetlList;
 import org.randoom.setlx.types.SetlObject;
 import org.randoom.setlx.types.SetlSet;
@@ -36,26 +35,38 @@ public class WebRequests {
                                                       "    this.cookies := cookies;\n" +
                                                       "}";
 
-    public static SetlObject post(State state, String targetUrl, HashMap<String, String> formData, SetlSet cookieData) throws SetlException {
-        Value classCandidate = state.findValue(CLASS_NAME);
-        if (classCandidate == Om.OM) {
-            Block block = ParseSetlX.parseStringToBlock(state, CLASS_CODE_RESPONSE);
-            block.execute(state);
-            classCandidate = state.findValue(CLASS_NAME);
+    public static SetlObject get(State state, String targetUrl, SetlSet queryParameterMap, SetlSet cookieData) throws SetlException {
+        Client client = ClientBuilder.newClient();
+        WebTarget target = client.target(targetUrl);
+
+        for (Value e : queryParameterMap) {
+            SetlList entry = (SetlList) e;
+            target = target.queryParam(entry.getMember(1).getUnquotedString(state), entry.getMember(2).getUnquotedString(state));
         }
 
-        if (classCandidate.isClass() == SetlBoolean.FALSE) {
-            throw new IncompatibleTypeException("Value bound to variable '" + CLASS_NAME + "' is not a class: " + classCandidate.toString(state));
-        }
+        Invocation.Builder request = target.request();
 
+        request = setCookieData(state, cookieData, request);
+
+        Response response = request.get();
+        return mapResponse(state, response);
+    }
+
+    public static SetlObject post(State state, String targetUrl, SetlSet formDataMap, SetlSet cookieData) throws SetlException {
         Client client = ClientBuilder.newClient();
         WebTarget target = client.target(targetUrl);
         Invocation.Builder request = target.request();
 
         request = setCookieData(state, cookieData, request);
 
+        HashMap<String, String> formData = new HashMap<>();
+        for (Value e : formDataMap) {
+            SetlList entry = (SetlList) e;
+            formData.put(entry.getMember(1).getUnquotedString(state), entry.getMember(2).getUnquotedString(state));
+        }
+
         Response response = request.post(Entity.form(new MultivaluedHashMap<>(formData)));
-        return mapResponse(state, (SetlClass) classCandidate, response);
+        return mapResponse(state, response);
     }
 
     private static Invocation.Builder setCookieData(State state, SetlSet cookieData, Invocation.Builder request) throws SetlException {
@@ -69,12 +80,23 @@ public class WebRequests {
         return request;
     }
 
-    private static SetlObject mapResponse(State state, SetlClass responseClass, Response response) throws SetlException {
+    private static SetlObject mapResponse(State state, Response response) throws SetlException {
+        Value classCandidate = state.findValue(CLASS_NAME);
+        if (classCandidate == Om.OM) {
+            Block block = ParseSetlX.parseStringToBlock(state, CLASS_CODE_RESPONSE);
+            block.execute(state);
+            classCandidate = state.findValue(CLASS_NAME);
+        }
+
+        if (classCandidate.isClass() == SetlBoolean.FALSE) {
+            throw new IncompatibleTypeException("Value bound to variable '" + CLASS_NAME + "' is not a class: " + classCandidate.toString(state));
+        }
+
         List<Value> argumentValues = new ArrayList<>();
         argumentValues.add(Rational.valueOf(response.getStatus()));
         argumentValues.add(new SetlString(response.readEntity(String.class)));
         argumentValues.add(mapCookies(state, response.getCookies().values()));
-        return (SetlObject) responseClass.call(state, argumentValues, null, null, null);
+        return (SetlObject) classCandidate.call(state, argumentValues, null, null, null);
     }
 
     private static SetlSet mapCookies(State state, Collection<NewCookie> responseCookies) {
